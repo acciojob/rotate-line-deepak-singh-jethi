98d74c5eda010c53626cab8d588fc0be9c340b6e
DEEPAK SINGH JETHI <134581381+deepak-singh-jethi@users.noreply.github.com>
2023-12-10 16:29:03 +0530

    created a submission

--- script.js
+++ script.js
@@ -4,7 +4,7 @@ const line = document.querySelector("#line");
 let angle = 0;
 
 setInterval(() => {
-  //rotate 0.36 degress
+  //rotate 2 degree
   angle += 2;
   line.style.transform = `rotate(${angle}deg)`;
 }, 20);
